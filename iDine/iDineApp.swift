a5b5af41b3a8f905c79900fc8f8c2ec2fb256bf6
Alejandro Sanabria <alesanabriav@gmail.com>
2023-02-06 12:49:35 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Alejandro Sanabria on 6/02/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
