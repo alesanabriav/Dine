2f80bee6c18a6d204b97e554fa03c30fc629c309
Alejandro Sanabria <alesanabriav@gmail.com>
2023-02-06 13:07:58 -0500

    [add] assets, models and bundle extension

---
+++ iDine/Extensions/Bundle.swift
@@ -0,0 +1,30 @@
+//
+//  Bundle.swift
+//  iDine
+//
+//  Created by Alejandro Sanabria on 6/02/23.
+//
+
+import Foundation
+
+extension Bundle {
+    
+    func Decode<T: Decodable>(_ type: T.Type, from file: String) -> T? {
+        
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            return nil
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            return nil
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode(type, from: data) else {
+            return nil
+        }
+        
+        return loaded
+    }
+}
